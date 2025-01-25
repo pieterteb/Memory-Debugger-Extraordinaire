d87b877dfc0486a0d0bce9bd20da53d470bfc0ff
pieterteb <pieterteb@gmail.com>
2025-01-25 15:15:33 +0100

    Added additional malloc warning.

--- src/MDE_malloc.c
+++ src/MDE_malloc.c
@@ -14,7 +14,9 @@
 extern void* MDE_malloc(size_t size) {
     void* ptr = malloc(size);
 
-    if (!ptr) {
+    if (!size) {
+        MDE_warn("Attempted to malloc memory block of 0 bytes.");
+    } else if (!ptr) {
         MDE_warn("Failed to malloc memory block of size %zu.", size);
     } else {
         MDE_alloced_add(ptr, size);
